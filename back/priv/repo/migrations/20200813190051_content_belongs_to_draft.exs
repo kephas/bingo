695ec6609a9cb9105d39272dd9c6824f144e516d
Pierre Thierry <pierre@nothos.net>
2020-08-13 22:18:58 +0200

    Fix migration

--- back/priv/repo/migrations/20200813190051_content_belongs_to_draft.exs
+++ back/priv/repo/migrations/20200813190051_content_belongs_to_draft.exs
@@ -3,7 +3,7 @@ defmodule Back.Repo.Migrations.ContentBelongsToDraft do
 
   def change do
 	alter table(:bingodraftcontents) do
-	  add :bingodraft_id, references(:bingodrafts)
+	  add :bingodraft_id, references(:bingodrafts, on_delete: :delete_all)
 	end
   end
 end

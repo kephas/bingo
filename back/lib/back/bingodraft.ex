4e84f71fc371084860c2954925573f925cc13d16
Pierre Thierry <pierre@nothos.net>
2020-08-20 22:01:20 +0200

    Add partial replace (no choices)

--- back/lib/back/bingodraft.ex
+++ back/lib/back/bingodraft.ex
@@ -15,4 +15,25 @@ defmodule Back.Bingodraft do
   def serializable_list() do
 	Repo.all(Back.Bingodraft) |> Repo.preload(:contents) |> Enum.map(&flatten/1)
   end
+
+  def replace_all(new_drafts) do
+	result = Repo.transaction(fn ->
+	  Repo.delete_all(Back.Bingodraft)
+	  new_drafts
+	  |> Enum.map(&unserialize/1)
+	  |> Enum.map(&Repo.insert/1)
+	end)
+
+	case result do
+	  {:ok, _} ->
+		:ok
+
+	  _ ->
+		:err
+	end
+  end
+
+  def unserialize(skeleton) do
+	%Back.Bingodraft{title: Map.get(skeleton, "title"), size: Map.get(skeleton, "size")}
+  end
 end
--- back/lib/back_web/controllers/draft_controller.ex
+++ back/lib/back_web/controllers/draft_controller.ex
@@ -1,12 +1,24 @@
 defmodule BackWeb.DraftController do
   use BackWeb, :controller
   alias Back.Bingodraft
+  import Jason, only: [decode: 1]
 
   def view(conn, _params) do
     json conn, Bingodraft.serializable_list
   end
 
-  def store(conn, _params) do
-	# stocke
+  def store(conn, params) do
+	%{"_json" => drafts} = params
+	case Bingodraft.replace_all(drafts) do
+	  :ok ->
+		conn
+		|> put_status(:ok)
+		|> json(%{status: "ok"})
+
+	  :err ->
+		conn
+		|> put_status(:internal_server_error)
+		|> json(%{status: "err"})
+	end
   end
 end
